8062ae9eb00dd0b5655cb3540d35b02e9b772f6c
sizer <shinse.tanaka@gmail.com>
2019-11-17 18:00:27 +0900

    add DashboardPage
    `yarn run ng g component pages/DashboardPage`

--- projects/ats/src/app/app.module.ts
+++ projects/ats/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardPageComponent } from './pages/dashboard-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardPageComponent
   ],
   imports: [
     BrowserModule,
