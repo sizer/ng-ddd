01f4a50d6974eda2d23b020fae104a9b518901c5
sizer <shinse.tanaka@gmail.com>
2019-11-17 18:11:46 +0900

    routing dashboard

--- projects/ats/src/app/app-routing.module.ts
+++ projects/ats/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardPageComponent } from './pages/dashboard-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
